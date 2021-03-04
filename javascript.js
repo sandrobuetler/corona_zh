d610c930f0b4afe0e12d16bf180b8a3ca3e6aa4c
Sandro Bütler <sandro.buetler@students.fhnw.ch>
2021-03-04 16:42:33 +0100

    first push with own made dotmap, and NOT functioning collection script

---
+++ javascript.js
@@ -0,0 +1,13 @@
+const buttons = document.querySelector('.buttons');
+const plzBereiche = Array.from(document.querySelectorAll('g'))
+    //.filter(plzBereich => isPlzBereich(plzBereich));
+
+function isPlzBereich(plzBereich) {
+    if (plzBereich.id != null && plzBereich.data != null){
+        return true
+    }else {
+        return false
+    }
+}
+
+console.log(plzBereiche)
