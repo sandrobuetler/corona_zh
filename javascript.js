54f671c13ae24997eb78b96a348e0e8c9ed38f55
Sandro Bütler <sandro.buetler@students.fhnw.ch>
2021-03-17 14:04:17 +0100

    Changes by jannikfrei

--- javascript.js
+++ javascript.js
@@ -2,15 +2,13 @@ const buttons = document.querySelector('.buttons');
 const plzBereiche = Array.from(document.querySelectorAll('.cls-5'))
     .filter(plzs => isPlz(plzs));
 
-console.log(plzBereiche)
-
-plzBereiche.forEach(plzs => {
+/*plzBereiche.forEach(plzs => {
     const newButton = document.createElement('button');
     newButton.dataset.id = plzs.id;
     newButton.innerHTML = plzs.dataset.name;
     newButton.addEventListener('click', showPlz);
     buttons.appendChild(newButton);
-});
+});*/
 
 function showPlz($event) {
     const selectedPlz = document.querySelector(`#${$event.target.dataset.id}`);
@@ -24,11 +22,11 @@ function isPlz(plz) {
 window.addEventListener('load', setup);
 async function setup() {
     const globalTemps = await getData();
-
-    console.log(globalTemps.plz)
-    console.log(globalTemps.date)
-    console.log(globalTemps.population)
-    console.log(globalTemps.newcases)
+    for( var i = 0; i<globalTemps.date.length; i++) {
+        if (globalTemps.date[i] == $('#datePicker').datepicker('getDate')) {
+            console.log(globalTemps.newcases[i])
+        }
+    }
 }
 async function getData() {
     const response = await fetch('https://raw.githubusercontent.com/openZH/covid_19/master/fallzahlen_plz/fallzahlen_kanton_ZH_plz.csv');
