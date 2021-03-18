120ebe3e8527c8db684f91f49b8c18e8941f5060
jannik1995 <jannik.frei@students.fhnw.ch>
2021-03-18 16:54:10 +0100

    Updateable Table

--- javascript.js
+++ javascript.js
@@ -1,17 +1,20 @@
 const colorarray = ['ff1000', 'ff1800', 'ff2000', 'ff2800', 'ff3000', 'ff3800', 'ff4000', 'ff4800', 'ff5000', 'ff5800', 'ff6000', 'ff6800', 'ff7000', 'ff7800', 'ff8000', 'ff8800', 'ff9000', 'ff9800', 'ffa000', 'ffa800', 'ffb000', 'ffb800', 'ffc000', 'ffc800', 'ffd000', 'ffd800', 'ffe000', 'ffe800', 'fff000', 'fff800', 'ffff00', 'f7ff00', 'efff00', 'e7ff00', 'dfff00', 'd7ff00', 'cfff00', 'c7ff00', 'bfff00', 'b7ff00', 'afff00', 'a7ff00', '9fff00', '97ff00', '8fff00', '87ff00', '7fff00', '77ff00', '6fff00', '67ff00', '5fff00', '57ff00', '4fff00', '47ff00', '3fff00', '37ff00', '2fff00', '27ff00', '1fff00', '17ff00']
 const reversed = colorarray.reverse();
+
 var utc = new Date().toJSON().slice(0,10).replace(/-/g,'/');
 var utc2 = new Date().toJSON().slice(0,10).replace(/-/g,'-');
 var dates_as_int = (Date.parse);
 const now = dates_as_int(utc)+86800000;
+
 document.getElementById("rangeControlledByDate").setAttribute("max", now);
-console.log(now)
 document.getElementById("dateControlledByRange").setAttribute("max", utc2);
-console.log(utc2)
 window.addEventListener('load', setup);
+window.addEventListener('load', tablefunction);
 document.getElementById("dateControlledByRange").setAttribute("max", utc2);
+
 async function setup() {
     const globalTemps = await getData();
+
     for (var i = 0; i < globalTemps.date.length; i++) {
         if (globalTemps.date[i] == document.getElementById("dateControlledByRange").value) {
             if (globalTemps.newcases[i].toString().length < 4 && globalTemps.plz[i].toString()!= "PLZ_uebrige" && globalTemps.plz[i].toString()!= "unbekannt") {
@@ -55,3 +58,23 @@ jQuery(function($) {
 });
 
 
+async function tablefunction() {
+    const globalTemps = await getData();
+    const arrayOne = globalTemps.date;
+    const arrayTwo = globalTemps.plz;
+    const arrayThree = globalTemps.newcases;
+    const arrayfour = globalTemps.population;
+
+    const maxSize = Math.max(arrayOne.length, arrayTwo.length, arrayThree.length, arrayfour.length);
+    $('#table-content').empty();
+    for (let i = 0; i < maxSize; i++) {
+        if (arrayOne[i] == document.getElementById("dateControlledByRange").value && globalTemps.plz[i].toString()!= "PLZ_uebrige" && globalTemps.plz[i].toString()!= "unbekannt") {
+            let row = $('<tr>');
+            row.append($('<td>').html(arrayOne[i]));
+            row.append($('<td>').html(arrayTwo[i]));
+            row.append($('<td>').html(arrayThree[i]));
+            row.append($('<td>').html(arrayfour[i]));
+            $('#table-content').append(row);
+        }
+    }
+}
